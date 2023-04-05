779d1e7c092b77418e47b28161974ea01c1fa378
shivakumar123143182 <92016905+shivakumar123143182@users.noreply.github.com>
2023-04-05 18:23:49 +0530

    Add files via upload
    First Uploaded file in GitHub

---
+++ helloworld.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{ 
+    int n;
+    
+    cin>>n;
+     if(n==2)
+     {
+    
+    cout<<"hello world!";
+     }
+     else{
+        cout<<"not 2 value";
+
+     }
+    
+    
+    return 0;
+}
